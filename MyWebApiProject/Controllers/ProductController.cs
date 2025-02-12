b67b4816c4041f40b496fe5223e4e57e7398d5b2
a.mohsen <a.mohsen@asis-it.com>
2025-02-12 14:17:52 +0200

    NewCHange

--- MyWebApiProject/Controllers/ProductController.cs
+++ MyWebApiProject/Controllers/ProductController.cs
@@ -14,6 +14,76 @@ namespace MyWebApiProject.Controllers
             return Ok();
         }
 
-        // Additional actions can be added here
+        // GET: api/product/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            // Logic to retrieve a product by ID
+            return Ok();
+        }
+
+        // POST: api/product
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            // Logic to create a new product
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+
+        // PUT: api/product/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, [FromBody] Product product)
+        {
+            // Logic to update an existing product
+            return NoContent();
+        }
+
+        // DELETE: api/product/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            // Logic to delete a product
+            return NoContent();
+        }
+
+        // GET: api/product/{id}/details
+        [HttpGet("{id}/details")]
+        public IActionResult GetProductDetails(int id)
+        {
+            // Logic to retrieve product details
+            return Ok();
+        }
+
+        // GET: api/product/{id}/reviews
+        [HttpGet("{id}/reviews")]
+        public IActionResult GetProductReviews(int id)
+        {
+            // Logic to retrieve product reviews
+            return Ok();
+        }
+
+        // POST: api/product/{id}/reviews
+        [HttpPost("{id}/reviews")]
+        public IActionResult CreateProductReview(int id, [FromBody] Review review)
+        {
+            // Logic to create a new product review
+            return CreatedAtAction(nameof(GetProductReviews), new { id = id }, review);
+        }
+
+        // PUT: api/product/{id}/reviews/{reviewId}
+        [HttpPut("{id}/reviews/{reviewId}")]
+        public IActionResult UpdateProductReview(int id, int reviewId, [FromBody] Review review)
+        {
+            // Logic to update an existing product review
+            return NoContent();
+        }
+
+        // DELETE: api/product/{id}/reviews/{reviewId}
+        [HttpDelete("{id}/reviews/{reviewId}")]
+        public IActionResult DeleteProductReview(int id, int reviewId)
+        {
+            // Logic to delete a product review
+            return NoContent();
+        }
     }
 }

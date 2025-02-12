3834cb136d72b2cc545249b6e7945bb519b57fc6
a.mohsen <a.mohsen@asis-it.com>
2025-02-12 14:02:35 +0200

    Changes

---
+++ MyWebApiProject/Migrations/20250212101739_UpdateEmployeeModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWebApiProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEmployeeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfJoining",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfJoining",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Employees");
+        }
+    }
+}
